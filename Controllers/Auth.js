54891cb6cdcff3d6bd46613c7dba0185150d5e09
Ajoy Kumar Pujari <ajoykumarpujari@gmail.com>
2023-06-06 08:16:51 +0530

    Login in  Function Added

--- Controllers/Auth.js
+++ Controllers/Auth.js
@@ -1,5 +1,7 @@
 const bcrypt =  require("bcrypt");
 const User = require("../model/User"); 
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 exports.signup = async (req, res)=> {
     try {
@@ -39,6 +41,77 @@ exports.signup = async (req, res)=> {
         return res.status(500).json({
             success: false,
             message: "User cannot be registered, please try again later",
+        });
+    }
+}
+
+
+//login
+exports.login = async (req, res) => {
+    try {
+        //fetch data from res.body
+        const {email, password} = req.body;
+        //validation email and password
+        if(!email || !password){
+            return res.status(400).json({
+                success:false,
+                message:"Please fill all the details Carefully",
+
+            });
+        }
+        //user available or not
+        const user = await User.findOne({email});
+        if(!user){
+            return res.status(401).json({
+                success:false,
+                message:"User not available ! please singup first",
+
+            });
+        }
+        //make payload
+        const payload = {
+            email: user.email,
+            id:user._id,
+            role:user.role,
+        };
+        //varify password & generate JWT TOKEN
+        if(await bcrypt.compare(password, user.password)){
+            //password match
+            //create json web token
+            let token = jwt.sign(payload,
+                                 process.env.JWT_SECRET,
+                                {
+                                    expiresIn: "2h"
+                                });
+            console.log(user);
+            //send the token in cookie
+            user.token = token;
+            console.log(user);
+            user.password =undefined;
+            console.log(user);
+            //create a cookie
+            const options = {
+                expiresIn: new Date(Date.now() + 3*24*60*60*1000),
+                httpOnly: true,   
+            }
+            res.cookie("token", token, options).status(200).json({
+                success: true,
+                token,
+                user,
+                message:"User Logged in Successfully",
+            });
+        }else{
+            return res.status(402).json({
+                success:false,
+                message:"Please enter a valid Password",
+
+            });
+        }
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({
+            success:false,
+            message:"Login Falure",
         });
     }
 }
